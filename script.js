c2224673a7e30f3e31206394e252562586749f35
PROTIK DAS UTSO <130892078+protik-das-utso@users.noreply.github.com>
2024-04-16 12:37:46 +0600

    Update date

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+let am_pm = document.querySelector(".am-pm");
+let week_day_out = document.querySelector(".week-day");
+let month_out = document.querySelector(".month");
+let date_out = document.querySelector(".date");
+let year_out = document.querySelector(".year");
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const month_name = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function updateTime() {
     const d = new Date();
     let hour = d.getHours();
     let minute = d.getMinutes();
     let second = d.getSeconds();
+    let week_day = d.getDay();
+    let month = d.getMonth();
+    let date = d.getDate();
+    let year = d.getFullYear();
 
     hour = hour = hour < 10 ? "0" + hour : hour;
     hour = hour > 12 ? hour - 12 : hour;
@@ -10,14 +23,27 @@ function updateTime() {
     minute = minute < 10 ? "0" + minute : minute;
     second = second < 10 ? "0" + second : second;
 
+    let am = hour < 12 ? "AM" : "PM";
+
     hour_set = document.querySelector(".hour");
     min_set = document.querySelector(".minute");
     sec_set = document.querySelector(".second");
 
+    am_pm.textContent = am;
+
+
     hour_set.textContent = hour + " : ";
     min_set.textContent = minute + " : ";
     sec_set.textContent = second;
+
+    week_day_out.textContent = weekday[week_day];
+    month_out.textContent = month_name[month];
+    date_out.textContent = date + " ,";
+    year_out.textContent = year;
+
 }
 
 updateTime();
 setInterval(updateTime, 100);
+
+
